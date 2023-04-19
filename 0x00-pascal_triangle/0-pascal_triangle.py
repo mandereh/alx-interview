55621706079a82879d2e0fae389b6b63438f2439
Daerego Braide <phronesis4xt@gmail.com>
2023-04-19 19:03:48 +0100

    task 0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""A script to determine pascal's triangle for any number"""
+
+
 def pascal_triangle(n):
+    """
+    returns a list of lists of integers representing the Pascal’s triangle of n
+    """
+    triangle = []
+
+    # return (trianlgle if n <= 0)
     if n <= 0:
-        return []
+        return triangle
+    for i in range(n):
+        temp_list = []
 
-    triangle = [[1]]
-    for i in range(1, n):
-        prev_row = triangle[i-1]
-        curr_row = [1]
-        for j in range(1, i):
-            curr_row.append(prev_row[j-1] + prev_row[j])
-        curr_row.append(1)
-        triangle.append(curr_row)
-
+        for j in range(i+1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(temp_list)
+    # print(triangle)
     return triangle
-
