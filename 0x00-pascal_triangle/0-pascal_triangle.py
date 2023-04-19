64e868085ff01c675d1dff88374ff36dca63715a
Daerego Braide <phronesis4xt@gmail.com>
2023-04-19 19:02:09 +0100

    task 0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,12 +2,15 @@
 def pascal_triangle(n):
     if n <= 0:
         return []
+
     triangle = [[1]]
     for i in range(1, n):
-        row = [1]
+        prev_row = triangle[i-1]
+        curr_row = [1]
         for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
-        triangle.append(row)
+            curr_row.append(prev_row[j-1] + prev_row[j])
+        curr_row.append(1)
+        triangle.append(curr_row)
+
     return triangle
 
